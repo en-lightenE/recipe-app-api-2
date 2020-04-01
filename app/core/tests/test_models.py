7cede99a3a2c0919f8e18a3bda830036073bcfe4
en-lightenE <validate81@gmail.com>
2020-04-01 16:39:25 -0400

    Added Core application flake8 and Travis

---
+++ app/core/tests/test_models.py
@@ -0,0 +1,9 @@
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+
+
+class ModelTests(TestCase):
+
+    def test_create_user_email_successful(self):
+        """Test creating a new user with an  email is successful"""
+         
