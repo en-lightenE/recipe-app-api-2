d2642fc3d4c492da8c93cf8c4a57ad28a15ec36d
en-lightenE <validate81@gmail.com>
2020-05-03 17:56:53 -0400

    Fix linting

--- app/recipe/tests/test_tags_api.py
+++ app/recipe/tests/test_tags_api.py
@@ -81,7 +81,7 @@ class PrivateTagsApiTests(TestCase):
         res = self.client.post(TAGS_URL, payload)
 
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
-    
+
     def test_retrieve_tags_assigned_to_recipes(self):
         """Test filtering tags by those assigned to recipes"""
         tag1 = Tag.objects.create(user=self.user, name='Breakfast')
